b6ea08bb53ee8ae78eb28af55dc00e9779fb4add
chitanuuu <86754783+chitanuuu@users.noreply.github.com>
2021-07-01 09:47:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-const CONFIG = {
-    introTitle: ' he luuu cậu!',
-    introDesc: `Tớ có điểu này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa`,
+const CONFIG = { Tình iu to đùng của bích ngọc <3
+    introTitle: ' he luuu !',
+    introDesc: `Chị có điểu này muốn hỏi em nhớ phải trả lời thật lòng nhaaa`,
     btnIntro: 'hihi',
-    title: 'Cậu yêu tớ phải không nào ._.',
-    desc: 'Nếu cậu thoát ra mà không trả lời tức là cậu muốn làm chồng tớ ',
+    title: 'Em yêu tớ phải không nào ._.',
+    desc: 'Nếu em thoát ra mà không trả lời tức là em muốn làm chồng chị ',
     btnYes: 'Yêuuuu ơi là yêuuuuu <33',
-    btnNo: 'Cậu mơ à :3',
-    question:'Nói cho tớ biết lý do cậu thích tớ đi <3',
+    btnNo: 'Chị mơ à :3',
+    question:'Nói cho tớ biết lý do em thích chị đi <3',
     btnReply: 'whyyyyyy <3',
-    reply: 'Tại vì tớ bị yêu câu từ cái nhìn đầu tiên ấyy <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h cậu qua đón tớ đi chơi nhé :33.',
+    reply: 'Tại vì em bị yêu câu từ cái nhìn đầu tiên ấyy <33333333',
+    mess: 'Chị biết mà 🥰. Yêu em 300.000 😘😘',
+    messDesc: 'Tối nay 7h em qua đón chị đi chơi nhé :33.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //linhttps://www.facebook.com/chitanuuu/
 }
